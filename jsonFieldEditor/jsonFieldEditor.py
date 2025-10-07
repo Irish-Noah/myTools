4abcf1b56602f87c91f1c344bb1a8b0fa46a8d24
Irish-Noah <obryanno@msu.edu>
2025-10-07 14:55:28 -0400

    added a gitignore for json files and added new functionality for a different policy use case

--- jsonFieldEditor/jsonFieldEditor.py
+++ jsonFieldEditor/jsonFieldEditor.py
@@ -5,9 +5,21 @@ import json
 import os
 
 def data_editor(data, old_source, new_source, signals): 
+	flags = ["ignore_E#x","persist-ref"]
+	for element in data: 
+		try:
+			if element['param'] in signals:
+				element['flags'] = flags
+			#print(element)
+		except KeyError:
+			pass
+			
+	return data
+
+def data_editor_source(data, old_source, new_source, signals): 
 	for element in data: 
 		if element['source'] == old_source:
-			if element['param'] in signals or 'BHCAN' in element['param'] or 'CCAN' in element['param'] or 'FDCAN8' in element['param']:
+			if element['param'] in signals:
 				element['source'] = new_source
 	return data
 
@@ -34,7 +46,7 @@ def main():
 	signals = ['ENGINE_TEMP_STAT', 'OILPRESSURESTS', 'FUELWATERPRESENTSTS', 'OIL_TEMP_IND_RQ', 'CHKFUELCAP', 'OILLIFELEFT', 'COOLENT_LEVEL_STAT', 'BRAKEFLUIDLEVELSTS', 'EPB_WARN_STAT', 'ELEC_STAB_STAT', 'EPS_STAT', 'ABSFAILSTS', 'AIRBAGFAILSTS', 'AIRBAGLAMP_FAILSTS', 'DAMPING_SYSTEM_STAT', 'TPM_INDLMPONRQ', 'BSS_LT_SRV_RQSTS', 'FCW_SYSTEM_STAT', 'MIL_ONRQ_BPCM', 'TURTLEMODESTS', 'HEV_LMP_RQ', 'HVBATCNTCTRSTAT']
 
 	#base['plans'][plan_to_edit]['filters'], signal_changes = filter_editor(base['plans'][plan_to_edit]['filters'], 'ref', 'persist-ref')
-	base['plans'][plan_to_edit]['data'] = data_editor(base['plans'][plan_to_edit]['data'], 'ref', 'persist-ref', signals)
+	base['plans'][plan_to_edit]['data'] = data_editor_source(base['plans'][plan_to_edit]['data'], 'ref', 'persist-ref', signals)
 
 	with open(output_path, 'w') as outfile:
 		json.dump(base, outfile, indent=2)
