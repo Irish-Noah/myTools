e3e33ae16b16f91c0f3f9cb269382f341d03c7a9
Irish-Noah <obryanno@msu.edu>
2025-10-24 12:11:04 -0400

    can read subdirs and files in this directory, organize file data to basic json objects

--- textToJSON/textToJson.py
+++ textToJSON/textToJson.py
@@ -1,22 +1,34 @@
+import os
 from pathlib import Path
 
 DIR_PATH = './'
 
-def create_json(file, report):
+def create_json(file):
     fp = open(file, 'r')
-    for line in fp:
-        print(line)
-        line = line.strip().replace('\\n','').replace('<>', '').replace(':', '')
-        devices = line.split("DEVICE")
-        print(devices)
+    content = fp.read()
+    content = content.strip().replace('\\n','').replace('<>', '')
+    devices = content.split('DEVICE')
+    #print(devices[1:])
     fp.close()
+    return devices
 
 
 def main(): 
-    report = []
-    for file in Path(DIR_PATH).rglob('*.txt'):
-        create_json(file, report)
-        break
+    boxes = {}
+
+    for subdir, dirs, files in os.walk(DIR_PATH):
+        for filename in files: 
+            if '.' in filename: 
+                continue
+
+            filepath = os.path.join(subdir, filename)
+            box = str(subdir).replace('./','')
+            if box in boxes.keys():
+                boxes[box][filename] = create_json(filepath)
+            else: 
+                boxes[box] = {filename : create_json(filepath)}
+            
+    print(boxes)
 
 
 if __name__ == "__main__":
